82565c67dca67ebaa90abe616faac0b35459d48e
AleksandraKazmierska <olaa.kazmierska@student.put.poznan.pl>
2024-02-29 11:15:15 +0100

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
